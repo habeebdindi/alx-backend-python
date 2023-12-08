fa00b0aee0169148182699b1ad7531c937b45cfc
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-08 03:13:45 +0100

    Correct linter

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,8 @@ from typing import Sequence, Union, Any, Mapping, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """Safely gets value"""
     if key in dct:
         return dct[key]
