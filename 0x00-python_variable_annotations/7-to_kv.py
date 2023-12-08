3a12aa81e3fabbb0531c1a2f9bfd7799d4d6b1c1
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-08 01:22:36 +0100

    Correct return annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,8 +3,8 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """function that takes a string k and an int OR float v as arguments.
        Returns a tuple.
     """
-    return (k, (v ** 2))
+    return (k, v ** 2)
