340df75c573a890092483a1431e5193c20cba391
Habeeb Dindi <habeebullahdindi@gmail.com>
2025-07-28 01:03:04 +0200

    (refactor): Implement paginate_users in  the same module as lazy_paginate

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,5 +1,15 @@
-#!/usr/bin/python3
-paginate_users = __import__('paginate-users').paginate_users
+#!/usr/bin/env python3
+seed = __import__('seed')
+
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
 
 def lazy_pagination(page_size):
     offset = 0
